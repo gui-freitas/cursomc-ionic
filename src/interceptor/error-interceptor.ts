e300e91792eeef5d0ab47fd7f398fc70c480b9fe
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2022-02-11 15:34:53 -0300

    Tratamento padrao para outros erros com Alert

--- src/interceptor/error-interceptor.ts
+++ src/interceptor/error-interceptor.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable  } from 'rxjs/Rx';
 import { StorageService } from '../services/storage.service';
+import { AlertController } from 'ionic-angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService){
+    constructor(
+        public storage: StorageService,
+        public alertCtrl: AlertController){
 
     }
 
@@ -26,17 +29,50 @@ export class ErrorInterceptor implements HttpInterceptor {
                 console.log(errorObj);
 
                 switch(errorObj.status){
+                    case 401:
+                        this.hadle401();
+                        break;
                     case 403:
                         this.handle403();
                         break;
+                    default:
+                        this.handleDefaultError(errorObj);
                 }
 
                 return Observable.throw(errorObj);
         }) as any;
     }
 
+    hadle401(){
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: falha de autenticação',
+            message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
     handle403(){
         this.storage.setLocalUser(null);
+    }
+
+    handleDefaultError(error){
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + error.status + ': ' + error.error,
+            message: error.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
     }
 }
 
